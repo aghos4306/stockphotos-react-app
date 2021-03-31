7a3773c3f66e1f42eb7323569b0b1930cf79d906
Aghogho Akpedeye <aghogho.akpedeye@gmail.com>
2021-03-31 12:51:57 +0300

    search query and code refactor

--- src/App.js
+++ src/App.js
@@ -11,20 +11,37 @@ const searchUrl = `https://api.unsplash.com/search/photos/`
 function App() {
   const [loading, setLoading] = useState(false)
   const [photos, setPhotos] = useState([])
-  const [page, setPage] = useState(1)
+  const [page, setPage] = useState(0)
+  const [query, setQuery] = useState('')
 
   const fetchImages = async () => {
     setLoading(true)
     let url
     const urlPage = `&page=${page}`
-    url = `${mainUrl}${clientID}${urlPage}`
+    const urlQuery = `&query=${query}`
+
+    if(query) {
+      url = `${searchUrl}${clientID}${urlPage}${urlQuery}`
+    } else {
+      url = `${mainUrl}${clientID}${urlPage}`
+    }
 
     try {
       const response = await fetch(url)
       const data = await response.json()
       setPhotos((oldPhotos) => {
-        return [...oldPhotos, ...data]
+        if(query && page === 1) {
+          return data.results
+        }
+        else if(query) {
+          return [...oldPhotos, ...data.results]
+        } else {
+          return [...oldPhotos, ...data]
+        }
       })
+     /*  setPhotos((oldPhotos) => {
+        return [...oldPhotos, ...data]
+      }) */
       setLoading(false)
     } catch (error) {
       setLoading(false)
@@ -34,6 +51,7 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault()
+    setPage(1)
     console.log('Hello')
   }
 
@@ -59,7 +77,13 @@ function App() {
     <main>
       <section className="search">
         <form className="search-form">
-          <input type="text" placeholder="search" className="form-input" />
+          <input 
+            type="text" 
+            placeholder="search" 
+            className="form-input" 
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
           <button type="submit" className="submit-btn" onClick={handleSubmit}>
             <FaSearch />
           </button>
